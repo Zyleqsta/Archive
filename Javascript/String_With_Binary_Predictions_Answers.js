8580a82e0482db0133b01904b661b884746d613d
Zyleqsta <contact@zyleqsta.com>
2022-01-30 00:01:35 -0800

    String Binary

---
+++ Javascript/String_With_Binary_Predictions_Answers.js
@@ -0,0 +1,11 @@
+alert('10' + '10'); // Prediction : Should equal 1010 // Correct
+alert('10' + 10); // Prediction : Should equal 1010 // Correct
+alert(10 + '10'); // Prediction : Should equal 1010 // Correct
+
+alert('6' - '6'); // Prediction : Should equal 0 // Correct
+alert('6' - 6); // Prediction : Should equal 0 // Correct
+alert(6 - '6'); // Prediction : Should equal 0 // Correct
+
+alert('5' / '3') // Prediction : Should equal 1 // Correct
+alert('5' / 3); // Prediction : Should equal 1 // Correct
+alert(5 / '3') // Prediction : Should equal 1 // Correct
