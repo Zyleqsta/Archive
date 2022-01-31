26c00a081ea6f82dcbb4637a360f561eab88f079
Zyleq <contact@zyleqsta.com>
2022-01-30 16:57:50 -0800

    Update Boolean_Stress.js

--- Javascript/Boolean_Stress.js
+++ Javascript/Boolean_Stress.js
@@ -1,11 +1,11 @@
 //Boolean Bullshit - So I can remeber this funnyness
-let a = 1; // Number
+let a = 0; // Number
 
-let b = "1"; // String
+let b = "0"; // String
 
 alert(Boolean(a)); // True
 alert(Boolean(b)); // True
 
 alert(a == b); // True? Whattttttttt!!!!!!!!!
 
-//Memory : This is happening because JavaScript is converting b("1") into a number making a == b (1 = 1).
+//Memory : This is happening because JavaScript is converting b("0") into a number making a == b (0 = 0).
